38d35ef9d72209e6c628c218c37471a206af4252
Ilaeth <przemek1999.10.5@wp.pl>
2021-09-30 19:05:46 +0200

    Zadanie 7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,12 @@ public class Main {
         Animal dog = new Animal("dog", 50.0, "Hawana");
         Animal cat = new Animal("cat", "Jinx");
 
-        Phone phone1 = new Phone("Samsung", "S10", 6.1, "android");
+        Phone phone1 = new Phone("Samsung", "S10", 2020, 6.1, "android");
 
         Human me = new Human("Przemek", "Podlecki", "123456789", dog);
 
-        Car car1 = new Car("X3", "BMW", "Black", 24000.0);
-        Car car2 = new Car("X3", "BMW", "Black", 24000.0);
+        Car car1 = new Car("X3", "BMW", 2012, "Black", 24000.0);
+        Car car2 = new Car("X3", "BMW", 2011, "Black", 24000.0);
 
         dog.feed();
         dog.takeForAWalk();
@@ -32,11 +32,9 @@ public class Main {
         me.setCar(car1);
         System.out.println(me.getCar());
 
-        System.out.println(new Car("X3", "BMW", "Black", 24000.0).equals(new Car("X3", "BMW", "Black", 24000.0)));
-        System.out.println(car2.equals(car1));
-        System.out.println(car2);
-        System.out.println(me);
-        System.out.println(dog);
+        System.out.println(car1);
         System.out.println(phone1);
+        phone1.turnOn();
+        car1.turnOn();
     }
 }
--- src/com/company/devices/Car.java
+++ src/com/company/devices/Car.java
@@ -1,24 +1,18 @@
 package com.company.devices;
 
-public class Car {
-    public final String model;
-    public final String producer;
+public class Car extends Device {
     public String color;
     public Double value;
 
-    public Car(String model, String producer, String color, Double value) {
-        this.model = model;
-        this.producer = producer;
+    public Car(String model, String producer, Integer yearofproduction, String color, Double value) {
+        super(model, producer, yearofproduction);
         this.color = color;
         this.value = value;
     }
 
     @Override
-    public String toString() {
-        return "Car{" +
-                "\nmodel='" + model + '\'' +
-                ", \nproducer='" + producer + '\'' +
-                ", \ncolor='" + color + '\'' +
-                ", \nvalue=" + value + '}';
+    public void turnOn() {
+        System.out.println("Naciskam Start");
     }
+
 }
